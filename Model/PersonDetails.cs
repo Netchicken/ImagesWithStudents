2ed9edefab80f454ebfccb46e14e8606c6bc61b0
Gary Dix <intelproof@gmail.com>
2025-03-26 12:07:15 +1300

    Details data added

---
+++ Model/PersonDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace ImagesWithStudents.Model
+{
+    public class PersonDetails
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Occupation { get; set; }
+        public string? ImagePath { get; set; }
+
+
+    }
+}
---
+++ Operations/StaticPersonDetails.cs
@@ -0,0 +1,23 @@
+﻿using ImagesWithStudents.Model;
+
+namespace ImagesWithStudents.Operations
+{
+    public static class StaticPersonDetails
+    {
+
+        public static List<PersonDetails> StaticAllStaff = new List<PersonDetails>
+{
+new PersonDetails {Id=1, Name ="John Window", Age= 28, Occupation="Builder", ImagePath = "photo1.jpg"},
+new PersonDetails {Id=2, Name = "Kelvin Wallbanger", Age = 32, Occupation="Baker", ImagePath = "photo2.jpg"},
+new PersonDetails {Id=3, Name = "Jessie Fenster", Age = 12, Occupation="Student", ImagePath = "photo3.jpg"},
+new PersonDetails {Id=4, Name = "Nancy Chao", Age = 11, Occupation="Student", ImagePath = "photo4.jpg"},
+new PersonDetails {Id=5, Name = "Mike Kartoffel", Age = 52, Occupation="Stuntman", ImagePath = "photo5.jpg"},
+new PersonDetails {Id=6, Name = "David Spiegel", Age = 61, Occupation="Accountant", ImagePath = "photo6.jpg"}
+};
+    }
+
+
+
+
+}
+
